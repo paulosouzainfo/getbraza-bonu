37d5031c2abe68a2c710dbacb814e66e55349cd7
Tiago Mello <tiagomello.info@icloud.com>
2025-02-28 10:42:05 -0300

    melhorias"

--- infra/config.py
+++ infra/config.py
@@ -6,11 +6,6 @@ from os import getenv
 dotenv.load_dotenv()
 
 class Config:
-    NAMESPACE = "getbraza-bonu"
-    MONGO_URI = getenv("MONGO_URI")
-    REDIS_URL = getenv("REDIS_URL")
-    REDIS_PORT = getenv("REDIS_PORT")
-
     DEVICE_ENDPOINT = getenv("DEVICE_ENDPOINT")
     STATEMENT_ENDPOINT = getenv("STATEMENT_ENDPOINT")
     COINS_ENDPOINT = getenv("COINS_ENDPOINT")
